c38d373c8ba091d62c859f305dc55d68ca5a8ca2
WP2_Dio Syani Dimastia <85340281+diosqd@users.noreply.github.com>
2021-09-21 12:32:33 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo    "<p><h2>Perkenalkan</h2></p>";
+        echo    "<p>Nama saya Dio Syani Dimastia</p>
+                <p>Saya tinggal di Cileungsi</p>
+                <p>hoby trading</p>";
+    }
+}
