7d1c7fbb18966905407a90db65d7f1e09b2c8c6c
WP2_Dio Syani Dimastia <85340281+diosqd@users.noreply.github.com>
2021-10-05 16:39:00 +0700

    pertemuan-4

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dio</li>
+        <li>Nama Belakang : Syani</li>
+        <li>Nama Lengkap : Dio Syani Dimastia</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Wakanda Forever , Saturnus, No.62121</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li> Planet Namex</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Joging</li>
+    </ul>
+</section>
