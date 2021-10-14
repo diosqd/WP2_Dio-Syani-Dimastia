fec6742bd64f2eae83dfd8d9a1d1462c345801a6
WP2_Dio Syani Dimastia <85340281+diosqd@users.noreply.github.com>
2021-10-14 19:07:20 +0700

    pertemuan-5 (form validation)

--- view-form-matakuliah.php
+++ view-form-matakuliah.php
@@ -5,6 +5,7 @@
 </head>
 
 <body>
+
     <center>
         <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
             <table>
@@ -22,15 +23,17 @@
                     <th>Kode MTK</th>
                     <th>:</th>
                     <td>
-                        <input type="text" name="kode" id="kode">
+                        <input type="text" name="kode" id="kode" value="<?php echo set_value('kode'); ?>">
                     </td>
+                    <td><?php echo form_error('kode'); ?></td>
                 </tr>
                 <tr>
                     <th>Nama MTK</th>
                     <td>:</td>
                     <td>
-                        <input type="text" name="nama" id="nama">
+                        <input type="text" name="nama" id="nama" value="<?php echo set_value('nama'); ?>">
                     </td>
+                    <td><?php echo form_error('nama'); ?></td>
                 </tr>
                 <tr>
                     <th>SKS</th>
@@ -43,6 +46,7 @@
                             <option value="4">4</option>
                         </select>
                     </td>
+                    <td><?php echo form_error('sks'); ?></td>
                 </tr>
                 <tr>
                     <td colspan="3" align="center">
