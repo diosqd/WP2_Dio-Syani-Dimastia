8e09445d5267b2ce50bf80b2bbede1b6e7a9a295
WP2_Dio Syani Dimastia <85340281+diosqd@users.noreply.github.com>
2021-11-19 15:54:51 +0700

    pertemuan-10

--- Autentifikasi.php
+++ Autentifikasi.php
@@ -58,30 +58,84 @@ class Autentifikasi extends CI_Controller
                         redirect('admin');
                     } else {
                         if ($user['image'] == 'default.jpg') {
-                            $this->session->set_flashdata(
-                                'pesan',
-                                '<div class="alert alert-info alert-message" role="alert">Silahkan 
-Ubah Profile Anda untuk Ubah Photo Profil</div>'
-                            );
+                            $this->session->set_flashdata('pesan', '<div class="alert alert-info alert-message" role="alert">Silahkan Ubah Profile Anda untuk Ubah Photo Profil</div>');
                         }
                         redirect('user');
                     }
                 } else {
-                    $this->session->set_flashdata('pesan', '<div 
-class="alert alert-danger alert-message" role="alert">Password 
-salah!!</div>');
+                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Password salah!!</div>');
                     redirect('autentifikasi');
                 }
             } else {
-                $this->session->set_flashdata('pesan', '<div 
-class="alert alert-danger alert-message" role="alert">User belum 
-diaktifasi!!</div>');
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">User belum diaktifasi!!</div>');
                 redirect('autentifikasi');
             }
         } else {
-            $this->session->set_flashdata('pesan', '<div 
-class="alert alert-danger alert-message" role="alert">Email tidak 
-terdaftar!!</div>');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Email tidak terdaftar!!</div>');
+            redirect('autentifikasi');
+        }
+    }
+
+    public function blok()
+    {
+        $this->load->view('autentifikasi/blok');
+    }
+    public function gagal()
+    {
+        $this->load->view('autentifikasi/gagal');
+    }
+
+    public function registrasi()
+    {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Lengkap',
+            'required',
+            [
+                'required' => 'Nama Belum diis!!'
+            ]
+        );
+        $this->form_validation->set_rules(
+            'email',
+            'Alamat Email',
+            'required|trim|valid_email|is_unique[user.email]',
+            [
+                'valid_email' => 'Email Tidak Benar!!',
+                'required' => 'Email Belum diisi!!',
+                'is_unique' => 'Email Sudah Terdaftar!'
+            ]
+        );
+        $this->form_validation->set_rules(
+            'password1',
+            'Password',
+            'required|trim|min_length[3]|matches[password2]',
+            [
+                'matches' => 'Password Tidak Sama!!',
+                'min_length' => 'Password Terlalu Pendek'
+            ]
+        );
+        $this->form_validation->set_rules('password2', 'Repeat Password', 'required|trim|matches[password1]');
+        if ($this->form_validation->run() == false) {
+            $data['judul'] = 'Registrasi Member';
+            $this->load->view('templates/aute_header', $data);
+            $this->load->view('autentifikasi/registrasi');
+            $this->load->view('templates/aute_footer');
+        } else {
+            $email = $this->input->post('email', true);
+            $data = [
+                'nama' => htmlspecialchars($this->input->post('nama', true)),
+                'email' => htmlspecialchars($email),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 0,
+                'tanggal_input' => time()
+            ];
+            $this->ModelUser->simpanData($data);
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Selamat!! akun member anda sudah dibuat. Silahkan Aktivasi Akun anda</div>');
             redirect('autentifikasi');
         }
     }
